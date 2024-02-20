20403ebc17dde6fb8ebc3478b9355d219df1d967
T876 <wondertate@gmail.com>
2024-02-20 09:40:41 -0700

    Search bar JS and minor site-wide style adjustments

---
+++ JS/main.js
@@ -0,0 +1,17 @@
+function searchImages() {
+    var input, filter, table, tableData, name, i, txtValue;
+    input = document.getElementById('img-search-bar');
+    filter = input.value.toUpperCase();
+    table = document.getElementById("image-gallery");
+    tableData = table.getElementsByClassName('gal-column');
+
+    for (i = 0; i < tableData.length; i++) {
+        name = tableData[i].getElementsByClassName("name")[0];
+        txtValue = name.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            tableData[i].style.display = "";
+        } else {
+            tableData[i].style.display = "none";
+        }
+      }
+}
