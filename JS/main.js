7cdb3e022e9c9de771b3306bfaade6687c98fb12
T876 <wondertate@gmail.com>
2024-02-27 22:31:52 -0700

    Added username of the user that's logged in to signed in pages

--- JS/main.js
+++ JS/main.js
@@ -15,3 +15,9 @@ function searchImages() {
         }
       }
 }
+
+function onInit() {
+    document.getElementById("username-display").innerText = "Welcome, " + localStorage.getItem('username')
+}
+
+onInit()
