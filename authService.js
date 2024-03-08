568056c313da4c93d37831800e8e64b13b091b4e
T876 <wondertate@gmail.com>
2024-03-08 14:29:37 -0700

    Basic setup for login backend

---
+++ authService.js
@@ -0,0 +1,13 @@
+function login(username, password) {
+    if (username && password) {
+        return true;
+    } else {
+        return false;
+    };
+}
+
+function getUserData(username) {
+
+}
+
+export {login, getUserData};
--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import {login, getUserData} from 'authService.js';
+
 const express = require('express');
 const app = express();
 
@@ -8,4 +10,9 @@ app.use((req, res, next) => {
     next();
 });
 
-app.use(express.static('public'));4
+app.use(express.static('public'));
+
+app.get('/login/:username-:password', (req, res) => {
+    let username, password = login(req.params.username, req.params.password);
+    res.send(username, password)
+})
