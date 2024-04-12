6a96332a18be6283ccbac0726220e4115555defe
T876 <wondertate@gmail.com>
2024-04-11 22:58:20 -0600

    Login page HTML

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -2,8 +2,25 @@ import React from 'react';
 
 export function Login() {
   return (
-    <main className='container-fluid bg-secondary text-center'>
-      <div>login displayed here</div>
+    <main className='container-fluid text-center'>
+      <h1>Welcome to MyRPGVisual!</h1>
+      <h5>Sign In:</h5>
+      <div id="create-account-form">
+          <div className="mb-3">
+            <label for="signinUsername" className="form-label">Username</label>
+            <input required type="email" className="form-control" id="signinUsername" aria-describedby="emailHelp"></input>
+          </div>
+          <div className="mb-3">
+            <label for="signinPassword" className="form-label">Password</label>
+            <input required type="password" className="form-control" id="signinPassword"></input>
+          </div>
+          <button onclick="authenticateUser()" className="btn btn-primary centered" id="login-button">Submit</button>
+          <a href="./HTML/create-account.html">
+              <button type="button" className="btn btn-secondary">Create Account</button>
+          </a>
+          <div id="authError">
+          </div>
+      </div>
     </main>
   );
 }
